82c39cbcc10ac0e40e127cd934361db08a116352
Mrxh <2929314899@qq.com>
2021-03-07 17:45:02 +0800

    week10

---
+++ Week 10/server.js
@@ -0,0 +1,45 @@
+const http = require('http')
+
+http.createServer((req,res)=>{
+    let body = []
+    req.on('error',err=>{
+        console.error(err)
+    }).on('data',chunk=>{
+        body.push(chunk)
+    }).on('end',()=>{
+        body = Buffer.concat(body).toString();
+        console.log('body:',body)
+        res.writeHead(200,{'Content-Type':'text/html'})
+        res.end(`<html maa=a>
+<head>
+    <style>
+        #container{
+            width: 500px;
+            height: 500px;
+            display: flex;
+            background-color: rgb(255,255,255);
+        }
+        #container #myid{
+            width: 200px;
+            height: 100px;
+            background-color: rgb(255,0,0);
+        }
+        #container .c1{
+            flex:1;
+            background-color: rgb(0,255,0);
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <div id="container">
+            <div id="myid"/>
+            <div class="c1"/>
+        </div>
+    </div>
+</body>
+</html>`);
+    })
+}).listen(8088)
+
+console.log('server start')
