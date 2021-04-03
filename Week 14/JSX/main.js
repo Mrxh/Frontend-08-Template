9f7e89a17660f52aaf111ed8b29783ce74ab08af
Mrxh <2929314899@qq.com>
2021-04-03 23:32:17 +0800

    'week14'

---
+++ Week 14/JSX/main.js
@@ -0,0 +1,80 @@
+function createElement(type,attributes,...children){
+        
+    let element = null;
+    if(typeof type === 'string')
+        element = new ElementWrapper(type);
+    else
+        element = new type;
+    for(let name in attributes){
+        element.setAttribute(name,attributes[name]);
+    }
+    for(let child of children){
+        if(typeof child === 'string')
+            child = new TextWrapper(child);
+        element.appendChild(child);
+    }
+    return element;
+}
+
+class ElementWrapper {
+    constructor(type){
+        this.root = document.createElement(type);
+    }
+
+    setAttribute(name,value){
+        this.root.setAttribute(name,value);
+    }
+
+    appendChild(child){
+        child.mountTo(this.root);
+    }
+
+    mountTo(parent){
+        parent.appendChild(this.root);
+    }
+}
+
+class TextWrapper {
+    constructor(type){
+        this.root = document.createTextNode(type);
+    }
+
+    setAttribute(name,value){
+        this.root.setAttribute(name,value);
+    }
+
+    appendChild(child){
+        child.mountTo(this.root);
+    }
+
+    mountTo(parent){
+        parent.appendChild(this.root);
+    }
+}
+
+class Div {
+    constructor(){
+        this.root = document.createElement('div');
+    }
+
+    setAttribute(name,value){
+        this.root.setAttribute(name,value);
+    }
+
+    appendChild(child){
+        child.mountTo(this.root);
+    }
+
+    mountTo(parent){
+        parent.appendChild(this.root);
+    }
+}
+
+
+let dom = <Div id="a">
+            <span>a</span>
+            <span>b</span>
+            <span>c</span>
+        </Div>
+
+dom.mountTo(document.body);
